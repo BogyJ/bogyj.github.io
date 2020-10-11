18245c078e6222ab796fd9cafd5b360fab4ceac5
Bogdan <jbogdan1998@hotmail.com>
2020-10-11 22:07:42 +0200

    Add responsive functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,21 @@ let navToggleButton = document.querySelector(".nav-toggle");
 
 navToggleButton.addEventListener("click", () => {
     let header = document.getElementById("header");
-    let sectionList = document.querySelector(".header__navigation-sections-list");
+    let sectionsList = document.querySelector(".header__navigation-sections-list");
     if (header.className.includes("nav-open")) {
-        sectionList.style.animation = "slideOut .4s linear";
-        sectionList.style.top = 0;
+        sectionsList.style.animation = "slideOut .4s linear";
+        sectionsList.style.top = 0;
     } else {
-        sectionList.style.right = 0;
-        sectionList.style.top = 0;
-        sectionList.style.animation = "slideInRight .4s linear";
+        sectionsList.style.right = 0;
+        sectionsList.style.top = 0;
+        sectionsList.style.animation = "slideInRight .4s linear";
     }
 
     header.classList.toggle("nav-open");
     header.classList.toggle("nav-closed");
 
     if (header.className.includes("nav-closed")) {
-        sectionList.style.right = "-100vw";
+        sectionsList.style.right = "-100vw";
     }
 
 });
