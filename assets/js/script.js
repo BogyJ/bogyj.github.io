496965d5ac4cadf79af613183c7a19a164c2e0a2
Bogdan <jbogdan1998@hotmail.com>
2020-10-12 11:26:12 +0200

    Add desktop responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,67 @@
-let navToggleButton = document.querySelector(".nav-toggle");
-let navLinks = document.querySelectorAll(".nav-links");
-
-function handleNavigationToggle() {
-    let header = document.getElementById("header");
-    let sectionsList = document.querySelector(".header__navigation-sections-list");
+(function() {
+    let navToggleButton = document.querySelector(".nav-toggle");
+    let navLinks = document.querySelectorAll(".nav-links");
+    let logo = document.querySelector(".home");
+    let scrollToTopBtn = document.querySelector(".scroll-to-top");
     
-    if (window.innerWidth >= 768) {
-        if (header.className.includes("nav-closed")) {
-            sectionsList.style.right = "0";
-            header.classList.remove("nav-closed");
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
         }
-        return;
     }
     
-    if (header.className.includes("nav-open")) {
-        sectionsList.style.animation = "slideOut .4s linear";
-        sectionsList.style.top = 0;
-    } else {
-        sectionsList.style.right = 0;
-        sectionsList.style.top = 0;
-        sectionsList.style.animation = "slideInRight .4s linear";
+    function scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     }
-
-    header.classList.toggle("nav-open");
-    header.classList.toggle("nav-closed");
-
-    if (header.className.includes("nav-closed")) {
-        sectionsList.style.right = "-100vw";
+    
+    scrollToTopBtn.addEventListener("click", scrollToTop);
+    logo.addEventListener("click", scrollToTop);
+    
+    function handleNavigationToggle() {
+        let header = document.getElementById("header");
+        let sectionsList = document.querySelector(".header__navigation-sections-list");
+        
+        if (window.innerWidth >= 1024) {
+            if (header.className.includes("nav-closed")) {
+                sectionsList.style.right = "0";
+                header.classList.remove("nav-closed");
+            }
+            return;
+        }
+        
+        if (header.className.includes("nav-open")) {
+            sectionsList.style.animation = "slideOut .4s linear";
+            sectionsList.style.top = 0;
+        } else {
+            sectionsList.style.right = 0;
+            sectionsList.style.top = 0;
+            sectionsList.style.animation = "slideInRight .4s linear";
+        }
+    
+        header.classList.toggle("nav-open");
+        header.classList.toggle("nav-closed");
+    
+        if (header.className.includes("nav-closed")) {
+            sectionsList.style.right = "-100vw";
+        }
     }
-}
-
-function init() {
-    if (window.innerWidth < 768) {
-        navToggleButton.addEventListener("click", handleNavigationToggle);
-        navLinks.forEach(el => {
-            el.addEventListener("click", handleNavigationToggle);
-        });
+    
+    function init() {
+        if (window.innerWidth < 1024) {
+            navToggleButton.addEventListener("click", handleNavigationToggle);
+            navLinks.forEach(el => {
+                el.addEventListener("click", handleNavigationToggle);
+            });
+        }
     }
-}
+    
+    window.addEventListener("load", init);
+    window.addEventListener("resize", handleNavigationToggle);
+    window.addEventListener("scroll", scrollFunction);
 
-window.addEventListener("load", init);
-window.addEventListener("resize", handleNavigationToggle);
+})();
+
 
